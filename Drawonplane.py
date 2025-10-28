273c714fd00bc993d3a2d3f455f0e476b7cb6962
KSUhippiebus2 <ksuhippiebus2@gmail.com>
2025-10-27 18:02:26 -0700

    Update Drawonplane.py

--- Drawonplane.py
+++ Drawonplane.py
@@ -1,8 +1,9 @@
 from modules.URrobot import UniversalRobot
+import RobotUrsila
 
 # --- Configuration ---
-UR_IP = "10.30.21.101"   # Replace with your robot's IP
-UR_PORT = 30002          # URScript TCP port
+UR_IP = RobotUrsila.UR_IP   # Replace with your robot's IP
+UR_PORT = RobotUrsila.UR_PORT          # URScript TCP port
 ACC = 1.2                # Acceleration (m/s^2)
 VEL = 0.25               # Velocity (m/s)
 COMMAND_WAIT = 0.5             # Time to wait between commands (s)
